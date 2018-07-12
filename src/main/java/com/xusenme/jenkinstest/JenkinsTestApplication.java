ffea5dcd8a6cf8659056803357f3cae8561f123c
xusenme <xusenme@163.com>
2018-07-12 14:28:52 +0800

    first commit

---
+++ src/main/java/com/xusenme/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,12 @@
+package com.xusenme.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+}
